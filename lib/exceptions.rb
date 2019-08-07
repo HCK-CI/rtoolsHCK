60ad17c27d559c517f755774928e3f139831e0f8
Bishara AbuHattoum <bishara@daynix.com>
2019-08-07 05:16:10 -0400

    Moving custom exceptions to a file with their own classes
    Signed-off-by: Bishara AbuHattoum <bishara@daynix.com>

---
+++ lib/exceptions.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+# A custom RToolsHCK error exception
+class RToolsHCKError < StandardError
+  # Custom addition to the exception backtrace, (better logging)
+  attr_reader :where
+
+  # Initialization of the custom exception
+  def initialize(where)
+    @where = where
+  end
+end
+
+# A custom RToolsHCK connection error exception
+class RToolsHCKConnectionError < RToolsHCKError; end
+
+# A custom RToolsHCK action error exception
+class RToolsHCKActionError < RToolsHCKError; end
+
+# A custom Winrm powershell run error exception
+class WinrmPSRunError < RToolsHCKActionError; end
