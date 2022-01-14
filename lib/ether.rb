f0a68a0aa7ee82479d3ea64802958dd91ca17706
Kostiantyn Kostiuk <konstantin@daynix.com>
2022-01-14 15:10:31 +0000

    Move server to ether
    Signed-off-by: Kostiantyn Kostiuk <konstantin@daynix.com>

--- lib/ether.rb
+++ lib/ether.rb
@@ -1,15 +1,18 @@
 # frozen_string_literal: true
 
 require_relative 'exceptions'
+require_relative 'server'
 require 'socket'
 
 # ether class
 class Ether
-  def initialize(init_opts)
+  def initialize(server_init_opts, init_opts)
+    @server_init_opts = server_init_opts
     @log_to_stdout = init_opts[:log_to_stdout]
     @stdout_logger = Logger.new($stdout) if @log_to_stdout
     @logger = init_opts[:logger]
     load_instance_variables(init_opts)
+    load_server
     logger('debug', 'initialize/ether') { "to #{@addr}:#{@port}" }
     load_ether
   rescue StandardError
@@ -18,6 +21,23 @@ class Ether
   end
 
   private
+
+  def get_exception_stack(exception)
+    exception.backtrace.select { |line| line.include?(File.dirname(__FILE__)) }\
+             .join("\n   -- ")
+  end
+
+  def log_exception(exception, level)
+    eclass = exception.class
+    emessage = exception.message
+    estack = get_exception_stack(exception)
+    if exception.is_a?(RToolsHCKError)
+      ewhere = exception.where
+      logger(level, ewhere) { "(#{eclass}) #{emessage}\n   -- #{estack}" }
+    else
+      logger(level, eclass) { "#{emessage}\n   -- #{estack}" }
+    end
+  end
 
   def logger(level, progname = nil, &block)
     @stdout_logger.public_send(level, progname, &block) if @log_to_stdout
@@ -29,6 +49,11 @@ class Ether
     @port = init_opts[:port]
     @timeout = init_opts[:timeout]
     @connection_timeout = init_opts[:connection_timeout]
+  end
+
+  def load_server
+    logger('debug', 'initialize/ether') { 'server is not running' } if @toolshck_server.nil?
+    @toolshck_server ||= Server.new(@server_init_opts)
   end
 
   def load_ether
@@ -83,6 +108,12 @@ class Ether
     data
   end
 
+  def unload_server
+    @toolshck_server&.close
+  rescue StandardError => e
+    log_exception(e, 'debug')
+  end
+
   # ether exit timeout
   ETHER_EXIT_TIMEOUT = 5
 
@@ -97,6 +128,7 @@ class Ether
   ensure
     logger('debug', 'close/ether') { 'closed' }
     @ether&.close
+    unload_server
   end
 
   def cmd(cmd, timeout = @timeout)
